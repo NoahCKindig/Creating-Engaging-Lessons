a49a1dc1cd0195b1c66363e432298c3918e5884f
NoahCKindig <140777705+NoahCKindig@users.noreply.github.com>
2023-07-27 23:19:30 -0400

    Creating Engaging Lessons for Learners
    Six Key Topics to create Engaging lessons, for both online and in-person learning.

---
+++ story_content/6QrYdQKRD45_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.78.30680.0%0D%0A%0D%0A00:00:00.352%20--%3E%2000:00:06.424%0D%0ANow%20that%20you%20know%20which%20topics%20you%20want%20to%20tackle,%20craft%20a%20respose%20for%20charlie%20to%20deal%20with%20the%20situation.%0D%0A%0D%0A"}')
